d3b952054c5348b788779be7ff2842b5753ed4c6
silvian <silvian.dragan@gmail.com>
2017-05-21 19:43:39 +0100

    updated path to main service location

--- alarm/alarm.sh
+++ alarm/alarm.sh
@@ -11,7 +11,7 @@
 ### END INIT INFO
 
 # Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/usr/local/bin/alarm-client/alarm
+DIR=/usr/local/src/alarm-client/alarm
 DAEMON=$DIR/alarm_service.py
 DAEMON_NAME=alarm_service
 
